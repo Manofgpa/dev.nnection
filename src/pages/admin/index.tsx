84f15d38e4684865de6c0029687daff744953cdf
Felipe Mano <manofgpa@gmail.com>
2021-10-12 19:30:59 -0300

    Add fetch user data

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -13,7 +13,9 @@ import {
   Td,
   Text,
   useBreakpointValue,
+  Spinner,
 } from '@chakra-ui/react'
+import { useQuery } from 'react-query'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
@@ -22,16 +24,19 @@ import Link from 'next/link'
 import { useEffect } from 'react'
 
 export default function Admin() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+
+    return data
+  })
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
 
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(res => res.json())
-      .then(data => console.log(data))
-  }, [])
+  useEffect(() => {}, [])
 
   return (
     <Flex direction='column' h='130vh' bg='gray.900'>
@@ -68,93 +73,105 @@ export default function Admin() {
               </Button>
             </Link>
           </Flex>
-          <Table colorScheme='blackAlpha'>
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color='gray.300' width='8'>
-                  <Checkbox colorScheme='green' />
-                </Th>
-                <Th>User</Th>
-                <Th>Registration date </Th>
-                <Th w='8'></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme='green' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Felipe Mano</Text>
-                    <Text fontSize='small' color='gray.300'>
-                      manofgpa@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>October 10, 2021</Td>}
-                <Td>
-                  <Button
-                    as='a'
-                    size='md'
-                    fontSize='md'
-                    colorScheme='orange'
-                    leftIcon={<Icon as={RiPencilLine} />}>
-                    Edit
-                  </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme='green' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Felipe Mano</Text>
-                    <Text fontSize='small' color='gray.300'>
-                      manofgpa@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>October 10, 2021</Td>}
-                <Td>
-                  <Button
-                    as='a'
-                    size='md'
-                    fontSize='md'
-                    colorScheme='orange'
-                    leftIcon={<Icon as={RiPencilLine} />}>
-                    Edit
-                  </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme='green' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Felipe Mano</Text>
-                    <Text fontSize='small' color='gray.300'>
-                      manofgpa@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>October 10, 2021</Td>}
-                <Td>
-                  <Button
-                    as='a'
-                    size='md'
-                    fontSize='md'
-                    colorScheme='orange'
-                    leftIcon={<Icon as={RiPencilLine} />}>
-                    Edit
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justify='center'>
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify='center'>
+              <Text>Fail to fetch data.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme='blackAlpha'>
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color='gray.300' width='8'>
+                      <Checkbox colorScheme='green' />
+                    </Th>
+                    <Th>User</Th>
+                    <Th>Registration date </Th>
+                    <Th w='8'></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme='green' />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight='bold'>Felipe Mano</Text>
+                        <Text fontSize='small' color='gray.300'>
+                          manofgpa@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>October 10, 2021</Td>}
+                    <Td>
+                      <Button
+                        as='a'
+                        size='md'
+                        fontSize='md'
+                        colorScheme='orange'
+                        leftIcon={<Icon as={RiPencilLine} />}>
+                        Edit
+                      </Button>
+                    </Td>
+                  </Tr>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme='green' />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight='bold'>Felipe Mano</Text>
+                        <Text fontSize='small' color='gray.300'>
+                          manofgpa@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>October 10, 2021</Td>}
+                    <Td>
+                      <Button
+                        as='a'
+                        size='md'
+                        fontSize='md'
+                        colorScheme='orange'
+                        leftIcon={<Icon as={RiPencilLine} />}>
+                        Edit
+                      </Button>
+                    </Td>
+                  </Tr>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme='green' />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight='bold'>Felipe Mano</Text>
+                        <Text fontSize='small' color='gray.300'>
+                          manofgpa@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>October 10, 2021</Td>}
+                    <Td>
+                      <Button
+                        as='a'
+                        size='md'
+                        fontSize='md'
+                        colorScheme='orange'
+                        leftIcon={<Icon as={RiPencilLine} />}>
+                        Edit
+                      </Button>
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Flex>
