0d6f236766402122f184ff4cafa7e7612fc50f74
Felipe Mano <manofgpa@gmail.com>
2021-10-14 12:16:25 -0300

    Add cookies refresh functionality

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,12 @@
-import axios from 'axios'
-import { parseCookies } from 'nookies'
+import axios, { AxiosError } from 'axios'
+import { parseCookies, setCookie } from 'nookies'
 
-const cookies = parseCookies()
+type User = {
+  token: string
+  refreshToken: string
+}
+
+let cookies = parseCookies()
 
 export const api = axios.create({
   baseURL: 'http://localhost:3001/',
@@ -9,3 +14,45 @@ export const api = axios.create({
     Authorization: `Bearer ${cookies['devnnection.token']}`,
   },
 })
+
+api.interceptors.request.use(
+  response => {
+    return response
+  },
+  (error: AxiosError) => {
+    if (error.response.status === 401) {
+      if (error.response.data?.code === 'token.expired') {
+        cookies = parseCookies()
+
+        const { 'devnnection.token': refreshToken } = cookies
+
+        api
+          .post<User>('refresh', {
+            refreshToken,
+          })
+          .then(response => {
+            const { token } = response.data
+
+            setCookie(undefined, 'devnnection.token', token, {
+              maxAge: 60 * 60 * 24 * 30, // 30 days
+              path: '/',
+            })
+
+            setCookie(
+              undefined,
+              'devnnection.refreshToken',
+              response.data.refreshToken,
+              {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/',
+              }
+            )
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`
+          })
+      }
+    } else {
+      // deslogar
+    }
+  }
+)
