6e3b8ef0a2ef66da0eb83e66ef2102cfec36d4f3
Felipe Mano <manofgpa@gmail.com>
2021-10-15 11:29:40 -0300

    Add nav pages

---
+++ src/pages/tags.tsx
@@ -0,0 +1,24 @@
+import { Box, Button, Text } from '@chakra-ui/react'
+import Link from 'next/link'
+import { setupAPIClient } from '../services/api'
+import { withSSRAuth } from '../utils/withSSRAuth'
+
+export default function Tags() {
+  return (
+    <Box position='fixed' top='50%' left='50%' fontSize='30' align='center'>
+      <Text m='2'>Under construction.</Text>
+      <Link href='/feed'>
+        <Button>Back to feed</Button>
+      </Link>
+    </Box>
+  )
+}
+
+export const getServerSideProps = withSSRAuth(async ctx => {
+  const apiClient = setupAPIClient(ctx)
+  const response = await apiClient.get('/me')
+
+  return {
+    props: {},
+  }
+})
